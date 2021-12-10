eebdc4dd78736f531e6658bee7c4fe0907c6af7a
Michael Messmore <michael.messmore@fedex.com>
2021-12-10 15:55:58 -0600

    Starting messaging

--- nats.go
+++ nats.go
@@ -4,11 +4,26 @@ import (
 	"github.com/nats-io/nats.go"
 	"github.com/prprprus/scheduler"
 )
+var nc, _ = nats.Connect(nats.DefaultURL)
 
-func Subscribers() {
-	nc, _ := nats.Connect(nats.DefaultURL)
+func publishOnline() {
+	nc.Publish("updatemgr.online", []byte(""))
+}
 
+func publishUpdatesAvailable() {
+	nc.Publish("updatemgr.updatesavailable", []byte(""))
+}
 
+func publishRebootRequired() {
+	nc.Publish("updatemgr.rebootrequired", []byte(""))
+}
+
+func ScheduledPublishers() {
+	s, _ := scheduler.NewScheduler(1000)
+
+	s.Delay().Minute(3).Do(publishOnline)
+	s.Delay().Minute(3).Do(publishUpdatesAvailable)
+	s.Delay().Minute(3).Do(publishRebootRequired)
 }
 
 
