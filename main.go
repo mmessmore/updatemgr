f4283969ae50e905ea6cce93ff06b34b199ff198
Michael Messmore <mike@messmore.org>
2021-12-27 08:39:56 -0600

    Starting to do config and stuff

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
+import "github.com/mmessmore/updatemgr/cmd"
 
 func main() {
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.Run()
+	cmd.Execute()
 }
